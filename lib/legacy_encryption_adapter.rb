849514472253c4ec9c6e1a7b4e762af410849134
Nick Benoit <nick.benoit@atomicjolt.com>
2022-06-07 08:21:32 -0600

    First

---
+++ lib/legacy_encryption_adapter.rb
@@ -0,0 +1,6 @@
+require "legacy_encryption_adapter/version"
+
+module LegacyEncryptionAdapter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/legacy_encryption_adapter/version.rb
@@ -0,0 +1,3 @@
+module LegacyEncryptionAdapter
+  VERSION = "0.1.0"
+end
